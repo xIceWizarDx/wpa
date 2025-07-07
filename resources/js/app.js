3667018aac52df0908a7d52d466a7b575ed853ce
gabrielmoraes102roo@gmail.com <96112719Gg.>
2025-07-06 22:11:22 -0400

    Atualização

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';     // carrega axios, Popper e configurações do Laravel
+import 'bootstrap';       // carrega o JavaScript do Bootstrap (depende de Popper)
+
+import Alpine from 'alpinejs';  
+window.Alpine = Alpine;  
+Alpine.start();
